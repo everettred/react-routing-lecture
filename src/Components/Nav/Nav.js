e6dac3ad3260379a35a455182a3eb959fa3f45bc
matias-perezferrero <matias.perezferrero@gmail.com>
2021-02-13 15:35:55 -0700

    first commit

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import logo from "./../../images/dm_white_logo.png";
+import "./Nav.css";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="Nav">
+        <div>
+          <img src={logo} alt="dm logo" />
+        </div>
+        <ul>
+          <Link to="/"><li>Home</li></Link>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Nav;
